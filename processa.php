291f6707e2fe1ea02c3c41fa3e2fb44f6b14ed47
cuelholima <matth.faria96@gmail.com>
2018-05-30 13:58:04 -0300

    visual e email

--- processa.php
+++ processa.php
@@ -1,30 +1,35 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-		$nome = $_POST['nome'];
-		$email = $_POST['email'];
-		$mensagem = $_POST['mensagem'];
-		
-        require 'vendor/autoload.php';
+<?php
 
-        $from = new SendGrid\Email(null, "matheuzinho.f@hotmail.com");
-        $subject = "Mensagem de contato";
-        $to = new SendGrid\Email(null, "matheuzinho.f@hotmail.com");
-        $content = new SendGrid\Content("text/html", "Olá Cesar, <br><br>Nova mensagem de contato<br><br>Nome: $nome<br>Email: $email <br>Mensagem: $mensagem");
-        $mail = new SendGrid\Mail($from, $subject, $to, $content);
-        
-        //Necessário inserir a chave
-        $apiKey = 'SENDGRID_API_KEY';
-        $sg = new \SendGrid($apiKey);
+if(!$_POST) exit;
 
-        $response = $sg->client->mail()->send()->post($mail);
-        echo "Mensagem enviada com sucesso";
-		
-        ?>
-    </body>
-</html>
+$email = $_POST['email'];
+
+
+//$error[] = preg_match('/\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $_POST['email']) ? '' : 'Preencha corretamente';
+if(!eregi("^[a-z0-9]+([_\\.-][a-z0-9]+)*" ."@"."([a-z0-9]+([\.-][a-z0-9]+)*)+"."\\.[a-z]{2,}"."$",$email )){
+	$error.="Preencha corretamente o email";
+	$errors=1;
+}
+if($errors==1) echo $error;
+else{
+	$values = array ('nome','email','telefone','mensagem');
+	$required = array('nome','email','telefone','mensagem');
+	 
+	$your_email = "contato@natureblow.com.br";
+	$email_subject = "Nova Mensagem do site: ".$_POST['subject'];
+	$email_content = "Nova Mensagem do site:\n";
+	
+	foreach($values as $key => $value){
+	  if(in_array($value,$required)){
+	    if( empty($_POST["$value"]) ) { echo 'Preencha corretamente '. $value; exit; }
+	  }
+	$email_content .= $value.': '.$_POST[$value]."\n";
+	}
+	 
+	if(@mail($your_email,$email_subject,$email_content)) {
+		echo 'Mensagem Enviada!'; 
+	} else {
+		echo 'ERROR!';
+	}
+}
+?>
